423ac812cd33c91b8d1730da712200d94643b278
Syed007Hassan <k201052@nu.edu.pk>
2022-08-16 01:23:53 +0500

    Initial V

---
+++ src/emojipedia.js
@@ -0,0 +1,69 @@
+import React from "react";
+
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym.",
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you.",
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for rolling on the floor, laughing.",
+  },
+  {
+    id: 4,
+    emoji: "🤔",
+    name: "Thinking Face",
+    meaning:
+      "The thinker's pose: Thoughtful face with the characteristic hand on the chin. Puts a statement, a person's intelligence, or an idea into question. Is mulling over something or thinking about a brilliant idea.",
+  },
+  {
+    id: 5,
+    emoji: "🤫",
+    name: "Exhorting Face",
+    meaning:
+      "Shhh! is meant to silence others. Or someone is confiding a secret that should not be retold. Nicely telling your counterpart to shut up.",
+  },
+  {
+    id: 6,
+    emoji: "😑",
+    name: "Expressionless Face",
+    meaning:
+      "Face without any emotional expression, with closed eyes and mouth. Tired, annoyed, emotionless, no patience, the end of the flagpole is reached. The right words are missing to continue a conversation.",
+  },
+  {
+    id: 7,
+    emoji: "😵‍💫",
+    name: "Face with spiral eyes",
+    meaning:
+      "The mouth is open, the eyes are spirals. The emoji has circulatory problems and is dizzy. Something or someone has cast a spell over us, we are hypnotized. There may be disorientation, e.g. in a foreign city or in a department store.",
+  },
+  {
+    id: 8,
+    emoji: "😷",
+    name: "Face With Medical Mask",
+    meaning:
+      "The mask is for protection. Either to protect yourself or others from infection. Someone is in the hospital, has to go to the doctor or has caught a disease. Wearing the masks is widely spread in Asia.",
+  },
+  {
+    id: 9,
+    emoji: "😈",
+    name: "Smiling Face With Horns",
+    meaning: 
+    "Mischievously grinning goblin smiley with small devil horns. Wants to cause trouble, is a real teaser or planning something ugly just now.",
+  },
+];
+
+export default emojipedia;
