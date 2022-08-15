6a9d18f93ae7e527cc220b2b35e68f2a49681534
Syed007Hassan <k201052@nu.edu.pk>
2022-08-16 01:32:19 +0500

    Updates

---
+++ precache-manifest.3dc3cdb6bea7f6a6fb62f7931224adbc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a90ebd4cb8f00a3238e424fe8ab6151",
+    "url": "/Emojipedia/index.html"
+  },
+  {
+    "revision": "be8c9388e0c2bb835496",
+    "url": "/Emojipedia/static/js/2.bc581622.chunk.js"
+  },
+  {
+    "revision": "2d006c54024aeb92b6c6",
+    "url": "/Emojipedia/static/js/main.b6c95b00.chunk.js"
+  },
+  {
+    "revision": "8e45ecf4ae811e339f7c",
+    "url": "/Emojipedia/static/js/runtime-main.ccd8d89c.js"
+  }
+]);
